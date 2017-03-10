905607c034f1dba8cdc32d40926c16e7b59ad43f
thanhnghi <lethanhnghi1402@gmail.com>
2017-03-10 14:04:22 +0700

    module_nghi

--- blocks/638/type-5.php
+++ blocks/638/type-5.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive Gas &amp; Oil Industry HTML Template">
-    <meta name="author" content="Gautam Thapar">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Responsive Gas &amp; Oil Industry HTML Template">
+        <meta name="author" content="Gautam Thapar">
 
-    <title>Offshore &#8211; Responsive Gas &amp; Oil Industry HTML Template</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- Important Owl stylesheet -->
-    <link rel="stylesheet" href="slider/owl-carousel/owl.carousel.css">
-    <!-- Default Theme -->
-    <link rel="stylesheet" href="slider/owl-carousel/owl.theme.css">
-    <!--  jQuery 1.7+  -->
-    <script src="slider/assets/js/jquery-1.9.1.min.js"></script>
-    <!-- Include js plugin -->
-    <script src="slider/owl-carousel/owl.carousel.js"></script>
+        <title>Offshore &#8211; Responsive Gas &amp; Oil Industry HTML Template</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Important Owl stylesheet -->
+        <link rel="stylesheet" href="slider/owl-carousel/owl.carousel.css">
+        <!-- Default Theme -->
+        <link rel="stylesheet" href="slider/owl-carousel/owl.theme.css">
+        <!--  jQuery 1.7+  -->
+        <script src="slider/assets/js/jquery-1.9.1.min.js"></script>
+        <!-- Include js plugin -->
+        <script src="slider/owl-carousel/owl.carousel.js"></script>
 
-    <?php
+        <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
         }
@@ -29,150 +29,55 @@
         ?>
 
         <link href="css/type-5.css" rel="stylesheet" type="text/css" />
-</head>
+    </head>
 
-<body>
-    <div class="type-5">
+    <body>
+        <div class="type-5">
 
-        <div class="title-our">
-            <h2 class="title1">OUR PARTNERS</h2>
-        </div>
-
-    <div class="clearfix" style="margin-top: 100px;"></div>
-
-    <div class="type_slider">
-        <div class="content">
-            <div id="owl-demo" class="owl-carousel owl-theme">
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner3.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner4.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner3.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner4.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner3.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner4.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner2.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner3.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner4.png" alt="" />
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="partner-images">
-                        <img src="images/partner1.png" alt="" />
-                    </div>
-                </div>
-                <script>
-                    $(document).ready(function() {
-
-                        $("#owl-demo").owlCarousel({
-                            //Set AutoPlay to 3 seconds
-
-                            items: 5
-
-                        });
-                    });
-                </script>
-
+            <div class="title-our">
+                <h2 class="title1">OUR PARTNERS</h2>
             </div>
+
+            <div class="clearfix" style="margin-top: 100px;"></div>
+
+            <section class="type_slider">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="item">
+                                <a href="#">
+                                    <img src="images/partner2.png"  alt="" >
+                                </a>
+                            </div>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="item">
+                                <a href="#">
+                                    <img src="images/partner3.png"  alt="" >
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="item">
+                                <a href="#">
+                                    <img src="images/partner4.png" alt="" >
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="item">
+                                <a href="#">
+                                    <img src="images/partner1.png" alt="" >
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>>
+
         </div>
-    </div>
-    <div class="clearfix" style="margin-top: 150px;"></div>
-</div>
-</body>
+    </body>
 
 </html>
--- blocks/639/type-5.php
+++ blocks/639/type-5.php
@@ -28,15 +28,15 @@
 </head>
 
 <body>
-        <div class="clearfix" style="margin-top: 150px;"></div>
+        
         <div class="type_footer">
             <footer>
 
                 <div class="footer">
                     <div class="container">
                         <!-- Prefooter Section -->
-                        <div class="row pre-footer eq-columns">
-                            <div class="widget widget-count-3 col-md-4 col-sm-12 col-xs-6">
+                        <section class="row pre-footer eq-columns">
+                            <div class="widget col-md-4 col-sm-12 col-xs-6">
                                 <div class="contact-box">
                                     <i class="fa fa-map-marker" aria-hidden="true"></i>
                                     <div class="contact-details">
@@ -46,7 +46,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="widget widget-count-3 col-md-4 col-sm-12 col-xs-6">
+                            <div class="widget  col-md-4 col-sm-12 col-xs-6">
                                 <div class="contact-box">
                                     <i class="fa fa-phone" aria-hidden="true"></i>
                                     <div class="contact-details">
@@ -56,7 +56,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="widget widget-count-3 col-md-4 col-sm-12 col-xs-6">
+                            <div class="widget  col-md-4 col-sm-12 col-xs-6">
                                 <div class="contact-box">
                                     <i class="fa fa-file-audio-o" aria-hidden="true"></i>
                                     <div class="contact-details">
@@ -67,14 +67,14 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </section>>
                         <!-- Prefooter Section -->
 
                         <!-- Footer widgets -->
-                        <div class="row widgets footer-widgets">
-                            <div class="widget widget-count-4 col-md-3 col-sm-6">
-                                <div class="textwidget">
-                                    <div class="about-txt"><img src="images/footer-logo.png" alt="logo"></div>
+                        <section class="row widgets footer-widgets">
+                            <div class="widget col-md-3 col-sm-6">
+                               
+                                    <img src="images/footer-logo.png" alt="logo"></
                                     <p></p>
                                     <p>Collaboratively deliver partnerships progressive alignments. Assertively premier supply chains before emerging solutions. Monetize high-payoff action items before wireless internal or "organic" sources exceptional action items. </p>
 
@@ -84,28 +84,28 @@
                                         <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                                         <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                                     </div>
-                                </div>
+                                
                             </div>
-                            <div class="widget widget-count-4 col-md-3 col-sm-6">
+                            <div class="widget  col-md-3 col-sm-6">
                                 <h2 class="widget-title">Quick Links</h2>
                                 <ul>
-                                    <li class="cat-item cat-item-2"><a href="#">Careers</a>
+                                    <li><a href="#">Careers</a>
                                     </li>
-                                    <li class="cat-item cat-item-3"><a href="#">Environment</a>
+                                    <li><a href="#">Environment</a>
                                     </li>
-                                    <li class="cat-item cat-item-4"><a href="#">Marketing &amp; Sales</a>
+                                    <li><a href="#">Marketing &amp; Sales</a>
                                     </li>
-                                    <li class="cat-item cat-item-6"><a href="#">Technology</a>
+                                    <li><a href="#">Technology</a>
                                     </li>
-                                    <li class="cat-item cat-item-7"><a href="#">The Company</a>
+                                    <li><a href="#">The Company</a>
                                     </li>
-                                    <li class="cat-item cat-item-8"><a href="#">Transportation</a>
+                                    <li><a href="#">Transportation</a>
                                     </li>
                                 </ul>
                             </div>
-                            <div class="widget widget-count-4 col-md-3 col-sm-6">
+                            <div class="widget  col-md-3 col-sm-6">
                                 <h2 class="widget-title">Our Services</h2>
-                                <div class="textwidget">
+                                
                                     <ul>
                                         <li> <a href="#"> Chemicals &amp; Commercial Fuels </a> </li>
                                         <li> <a href="#"> Aviation Fuels &amp; Marine Fuels </a> </li>
@@ -113,24 +113,24 @@
                                         <li> <a href="#"> Liquified Petrolium Gas </a> </li>
                                         <li> <a href="#"> Shell Sulphur, Trading &amp; Supply </a> </li>
                                     </ul>
-                                </div>
+                               
                             </div>
-                            <div class="widget widget-count-4 col-md-3 col-sm-6">
+                            <div class="widget  col-md-3 col-sm-6">
                                 <div class="newsletter widget">
                                     <h2 class="widget-title">Newsletter</h2>
                                     <p>Subscribe to our newsletters to receive latest news and updates.</p>
-                                    <form id="offshore-mc-form" novalidate="true">
+                                    <form  novalidate="true">
                                         <div class="form-group">
-                                            <input type="email" id="mc-email" name="EMAIL" placeholder="Enter your email">
+                                            <input type="email"  name="EMAIL" placeholder="Enter your email">
                                         </div>
-                                        <button type="submit" class="btn btn-block">Subscribe Now!</button>
+                                        <button type="submit" class=" btn-block">Subscribe Now!</button>
                                     </form>
-                                    <p id="offshore-mc-result" style="display:none;"></p>
-                                    <a id="offshore-mc-try" href="javascript:void(0)" style="display:none;">Try again</a>
+                                    <p style="display:none;"></p>
+                                    <a href="javascript:void(0)" style="display:none;">Try again</a>
                                 </div>
 
                             </div>
-                        </div>
+                        </section>>
                         <!-- Footer widgets -->
                     </div>
                 </div>
@@ -142,20 +142,20 @@
 
                             <div class="col-md-6">
                                 <div class="widget footer-widgets footer-bottom-widgets">
-                                    <div class="textwidget">Copyright © 2016 Offshore Industries. All rights reserved.</div>
+                                    <div >Copyright © 2016 Offshore Industries. All rights reserved.</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 text-right">
+                            <div class="col-md-6 ">
                                 <div class="widget footer-widgets footer-bottom-widgets">
-                                    <div class="menu-footer-bottom-container">
-                                        <ul id="menu-footer-bottom" class="menu">
-                                            <li id="menu-item-156" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-156"><a href="#">Terms of use</a></li>
-                                            <li id="menu-item-157" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-157"><a href="#">Privacy Policy</a></li>
+                                    
+                                        <ul >
+                                            <li ><a href="#">Terms of use</a></li>
+                                            <li  ><a href="#">Privacy Policy</a></li>
                                         </ul>
-                                    </div>
+                                   
                                 </div>
                                 <div class="widget footer-widgets footer-bottom-widgets">
-                                    <div class="textwidget"><span> | </span> Made with <i class="fa fa-heart" aria-hidden="true"></i> by Surjith S M &amp; Gautam Thapar</div>
+                                    <div><span> | </span> Made with <i class="fa" aria-hidden="true"></i> by Surjith S M &amp; Gautam Thapar</div>
                                 </div>
                             </div>
 
@@ -164,8 +164,7 @@
                 </div>
                 <!-- Copyright -->
             </footer>
-            <!-- back-to-top link -->
-            <a href="#" class="cd-top cd-is-visible cd-fade-out">Top</a>
+
         </div>
     </div>
 </body>
