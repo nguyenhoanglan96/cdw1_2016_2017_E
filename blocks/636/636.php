67c1b292869b15c19f5eaa7261c47a1c7d509a94
Văn Công <luvias125i@gmail.com>
2017-03-24 17:18:22 +0700

    up_moi

---
+++ blocks/636/636.php
@@ -0,0 +1,239 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>636</title>
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/636.less', 'css/636.css');
+        ?>
+
+        <link href="css/636.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <div class="type-636">
+            <div class="container warpper-text">
+                <div class="row">
+                    <!-- Page Title -->
+                    <div class="page-title text-center">
+                        <h2 class="title">SOCIAL RESPONIBILITY</h2>
+                        <p class="description light">
+                            By <a href="#">Taylor White</a> on Jun 01 in 
+                            <a href="#" rel="category tag">Transportation</a>.
+                        </p>
+                    </div>
+                    <!-- Page Title -->
+                    <!-- Breadcrumbs -->
+                    <div class="breadcrumbs">
+                        <div class="container">
+                            <span class="parent">
+                                <i class="fa fa-home"></i>
+                                <a href="#">Home</a>
+                            </span>
+                            <i class="fa fa-chevron-right"></i>
+                            <span class="child"> 
+                                <a href="#">Transportation</a>
+                            </span>
+                            <i class="fa fa-chevron-right"></i>
+                            <span class="child">SOCIAL RESPONIBILITY</span>
+                        </div>
+                    </div>
+                    <!-- Main Container -->
+                    <div class="text-icon">
+                        <div class="col-md-9 content">
+                            <article>
+                                <h4 class="subtitle">
+                                    Uniquely transform premier infrastructures before functional metrics. 
+                                    Completely mesh sustainable leadership for economically sound sources.
+                                </h4>
+                                <p>
+                                    Distinctively fashion standardized communities vis-a-vis seamless applications. 
+                                    Authoritatively recaptiualize efficient supply chains without vertical initiatives. 
+                                    Progressively visualize strategic relationships with error-free processes. 
+                                    Credibly strategize fully tested outsourcing with functional e-markets. 
+                                    Dynamically evisculate user friendly architectures before cross-media experiences. 
+                                    Interactively disintermediate empowered data whereas distinctive human capital. 
+                                    Compellingly utilize.
+                                </p>
+                                <p>
+                                    Dynamically utilize intermandated materials through customized process improvements. 
+                                    Conveniently recaptiualize adaptive leadership before long-term high-impact 
+                                    functionalities. Completely incubate customer directed meta-services after economically 
+                                    sound imperatives. Distinctively evolve diverse applications for enterprise processes. 
+                                    Monotonectally disintermediate functionalized solutions rather than 
+                                    progressive leadership.
+                                </p>
+                                <div class="cta">
+                                    <h2>
+                                        Wealth can only be accumulated by the earnings of industry and the savings of 
+                                        frugality.
+                                    </h2>
+                                </div>
+                                <h2 class="title-2">The Conclusion</h2>
+                                <p>
+                                    Continually myocardinate reliable channels with distributed networks. 
+                                    Monotonectally scale end-to-end bandwidth for ethical e-markets. Enthusiastically 
+                                    disintermediate low-risk high-yield ideas for robust outsourcing. 
+                                    Monotonectally provide access to corporate data through diverse materials. 
+                                    Collaboratively empower resource maximizing experiences and progressive meta-services. 
+                                    Energistically engage quality leadership for cooperative services. 
+                                    Energistically leverage existing client-centered experiences via virtual solutions. 
+                                    Compellingly conceptualize professional e-tailers through efficient action items. 
+                                    Credibly seize pandemic ROI with client-centered applications.
+                                </p>
+                                <p>
+                                    Interactively mesh client-centered content whereas interdependent niche markets. 
+                                    Appropriately iterate one-to-one web-readiness rather than impactful supply.
+                                </p>
+                                <div class="social-share text-center">
+                                    <a class="fb-share" href="#"> 
+                                        <i class="fa fa-facebook" aria-hidden="true"></i>Share on Facebook
+                                    </a>
+                                    <a class="tweet-share" href="#"> 
+                                        <i class="fa fa-twitter" aria-hidden="true"></i>Share on Twitter
+                                    </a>
+                                </div>
+                                <div class="author-box">
+                                    <div class="img">
+                                        <img src="images/1234.jpeg" srcset="" class="photo" height="118" width="118"/>
+                                    </div>
+                                    <div class="author-details">
+                                        <h4 class="subtitle">Taylor White</h4>
+                                        <p>
+                                            Continually myocardinate reliable channels with distributed networks. 
+                                            Monotonectally scale end-to-end bandwidth for ethical e-markets.
+                                        </p>
+                                        <div class="author-share">
+                                            <a href="#">
+                                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="#">
+                                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="#">
+                                                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="#">
+                                                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="#">
+                                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                            </a> 
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="comments" class="comments">
+                                </div>
+                                <div class="comment-box">
+                                    <div class="comment-respond">
+                                        <h3 class="comment-reply-title">Add Comment 
+                                            <small>
+                                                <a rel="nofollow" href="#" style="display:none;">
+                                                    Cancel reply
+                                                </a>
+                                            </small>
+                                        </h3>
+                                        <form action="#" method="post" class="commentform row">
+                                            <div class="col-md-12">
+                                                <textarea class="form-control" id="comment" name="comment" aria-required="true" placeholder="Message" textarea=""></textarea>
+                                            </div>
+                                            <div id="comment-name" class="col-md-4">
+                                                <input type="text" class="form-control" placeholder="Name" name="author" value="">
+                                            </div>
+                                            <div class="col-md-4">
+                                                <input type="email" class="form-control" placeholder="Email" name="email" value="">
+                                            </div>
+                                            <div id="comment-url" class="col-md-4">
+                                                <input type="url" id="url" class="form-control" placeholder="Website" name="url" value="">
+                                            </div>
+                                            <p class="form-submit">
+                                                <input name="submit" type="submit" id="submit" class="btn btn-block" value="Add Comment">
+                                                <input type="hidden" name="comment_post_ID" value="1333">
+                                                <input type="hidden" name="comment_parent" value="0">
+                                            </p>
+                                        </form>
+                                    </div>
+                                    <!-- #respond -->
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-md-3 sidebar">
+                            <div class="widget">
+                                <form method="get" action="#">
+                                    <div class="sidebar-search-form">
+                                        <input type="text" class="form-control" placeholder="Search" name="s" id="s">
+                                        <button class="btn search-btn" type="button">
+                                            <span class="fa fa-search"></span>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="widget widget_categories">
+                                <h3 class="sidebar-title">Categories</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">Careers</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Environment</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Marketing &amp; Sales</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Technology</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">The Company</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Transportation</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="widget">
+                                <div class="sidebar-download">
+                                    <div class="download-file">
+                                        <a href="#" download="Test.pdf">
+                                            <i class="fa fa-file-pdf-o" aria-hidden="true"></i> 
+                                            Download<span>17.18 KB</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <div class="sidebar-fact">
+                                    <h3 class="">Quick Fact</h3>
+                                    <p>Distinctively fashion in the effect of standard products communities via seamless 
+                                        applications. Authoritatively recaptiualize.</p>
+                                    <a href="#" class="btn btn-primary " role="button">Call To Action</a>
+                                </div>
+                            </div>
+                            <div id="tag_cloud-1" class="widget ">
+                                <h3 class="sidebar-title">Tags</h3>
+                                <div class="tagcloud"><a href="#" title="2 topics" style="font-size: 22pt;">Careers</a>
+                                    <a href="#" title="1 topic" style="font-size: 8pt;">Environment</a>
+                                    <a href="#" title="1 topic" style="font-size: 8pt;">Marketing &amp; Sales</a>
+                                    <a href="#" title="1 topic" style="font-size: 8pt;">Technology</a>
+                                    <a href="#" title="1 topic" style="font-size: 8pt;">The Company</a>
+                                    <a href="#" title="1 topic" style="font-size: 8pt;">Transportation</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Main Container -->
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
