faea1b6653e7035de415f2cd786b2137f9cd0d37
nguyenhoanglan96 <kohiunoi1996@gmail.com>
2017-04-21 16:06:25 +0700

    up

--- blocks/691/js/javascript.js
+++
@@ -1,6 +0,0 @@
- $('.navbar .dropdown').hover(function() {
-		  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
-		}, function() {
-		  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(105);
-		}
-);
